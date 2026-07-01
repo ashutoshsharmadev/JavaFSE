b00d186ee6fcdcc36c4ca09527216d425616242f
ashutoshsharmadev <ashutoshsharma.0499@gmail.com>
2026-07-01 23:29:13 +0530

    Module 4: Excercise 2 writing basic JUnit tests

--- JUnit/src/test/java/CalculatorTest.java
+++ JUnit/src/test/java/CalculatorTest.java
@@ -3,12 +3,39 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
-        @Test
-        public void addsTwoNumbers() {
-            Calculator calculator = new Calculator();
 
-            int result = calculator.add(2, 3);
+    Calculator calculator = new Calculator();
+    @Test
+    public void addsTwoNumbers() {
+//            Calculator calculator = new Calculator();
 
-            assertEquals(5, result);
-        }
+        int result = calculator.add(2, 3);
+
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void subtractsTwoNumbers() {
+
+        int result = calculator.subtract(2, 3);
+
+        assertEquals(-1, result);
+
+    }
+    @Test
+    public void multiplyTwoNumbers() {
+
+        int result = calculator.multiply(2, 3);
+
+        assertEquals(6, result);
+
+    }
+    @Test
+    public void dividesTwoNumbers() {
+
+        int result = calculator.divide(5, 5);
+
+        assertEquals(1, result);
+
+    }
 }
