5a8fe2a14a489418363af8b575189bf2ba865227
ashutoshsharmadev <ashutoshsharma.0499@gmail.com>
2026-07-02 00:06:14 +0530

    Module 4: Excercise 4 AAA Arrange Act Assert pattern, setup and teardown methods

--- JUnit/src/test/java/CalculatorTest.java
+++ JUnit/src/test/java/CalculatorTest.java
@@ -1,41 +1,68 @@
 import org.ashutosh.Calculator;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    Calculator calculator = new Calculator();
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setting up test........ ");
+        calculator = new Calculator();
+    }
+
     @Test
     public void addsTwoNumbers() {
-//            Calculator calculator = new Calculator();
-
-        int result = calculator.add(2, 3);
-
-        assertEquals(5, result);
+        // Arrange
+        int a = 10;
+        int b = 20;
+        // Act
+        int result = calculator.add(a, b);
+        // Assert
+        assertEquals(30, result);
     }
 
     @Test
     public void subtractsTwoNumbers() {
-
+        // Arrange
+        int a = 10;
+        int b = 20;
+        // Act
         int result = calculator.subtract(2, 3);
-
+        // Assert
         assertEquals(-1, result);
 
     }
     @Test
     public void multiplyTwoNumbers() {
-
+        // Arrange
+        int a = 10;
+        int b = 20;
+        // Act
         int result = calculator.multiply(2, 3);
-
+        // Assert
         assertEquals(6, result);
 
     }
     @Test
     public void dividesTwoNumbers() {
-
+        // Arrange
+        int a = 10;
+        int b = 20;
+        // Act
         int result = calculator.divide(5, 5);
-
+        // Assert
         assertEquals(1, result);
 
     }
+
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning up........ ");
+        calculator = null;
+    }
+
 }
