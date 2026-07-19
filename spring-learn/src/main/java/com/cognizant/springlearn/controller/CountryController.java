68370647dbc99e9c377491672886070db84c2334
ashutoshsharmadev <ashutoshsharma.0499@gmail.com>
2026-07-19 18:14:45 +0530

    Module 7: spring-rest-handson: REST - Country Web Service 

---
+++ spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,31 @@
+package com.cognizant.springlearn.controller;
+
+
+import com.cognizant.springlearn.Country;
+import com.cognizant.springlearn.SpringLearnApplication;
+import com.cognizant.springlearn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("country", Country.class);
+        context.close();
+        return country;
+    }
+    
+}
