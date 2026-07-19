4b01879329074e05cd8944ea4b75711a03ab8507
ashutoshsharmadev <ashutoshsharma.0499@gmail.com>
2026-07-19 17:04:29 +0530

    Module 7: spring-rest-handson: Load Country from Spring Configuration XML

--- spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -2,15 +2,30 @@ package com.cognizant.springlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
+
     public static void main(String[] args) {
         System.out.println("Starting springboot application");
         SpringApplication.run(SpringLearnApplication.class, args);
-
+        displayCountry();
         System.out.println("Springboot Application started successfully. Congrats");
+    }
+
+    public static void displayCountry() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("country", Country.class);
+        LOGGER.debug("Country : {}", country.toString());
     }
 
 }
